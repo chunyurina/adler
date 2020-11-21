ac3b2bfa4c92de8e79b6b55201a3afd31997634b
Daisuke073 <cpxyh073@gmail.com>
2020-11-21 16:50:52 +0900

    データ取得できました。

---
+++ app/controllers/scrapings_controller.rb
@@ -0,0 +1,10 @@
+class ScrapingsController < ApplicationController
+  # require 'mechanize'
+
+  def index
+    agent = Mechanize.new
+    page = agent.get('https://techacademy.jp/')
+    @elements = page.body
+  end
+
+end
