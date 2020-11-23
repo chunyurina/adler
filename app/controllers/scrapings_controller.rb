8627546e93fdc1ad3b967a38a10e4c96d826c227
Daisuke073 <cpxyh073@gmail.com>
2020-11-23 21:09:53 +0900

    いじりました

--- app/controllers/scrapings_controller.rb
+++ app/controllers/scrapings_controller.rb
@@ -5,12 +5,13 @@ class ScrapingsController < ApplicationController
     agent = Mechanize.new
     page = agent.get('https://www.jstage.jst.go.jp/browse/jjpsy/-char/ja')
     @elements = page.search('//*[@id="most-popular-articles-list"]/li[1]')
-    @elements.get_attribute(:href)
-    # @elements = page.search('')
+    # @elements.get_attribute(:href)
+    # # @elements = page.search('')
 
-    # @elements.each do |ele|
-    #   @hoge = ele.inner_text
-    # end
+    @elements.each do |ele|
+      @hoge = ele.(:class => 'clickable_div')
+      
+    end
 
   end
 end
