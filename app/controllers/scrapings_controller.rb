17fa58141c6e9e20d3af3ab3c5e08f0932ca3d98
chunyurina <yurina8ru@gmail.com>
2020-11-24 20:43:36 +0900

    とりあえずスクレイピングできました

--- app/controllers/scrapings_controller.rb
+++ app/controllers/scrapings_controller.rb
@@ -3,16 +3,15 @@ class ScrapingsController < ApplicationController
 
   def index
     agent = Mechanize.new
-    page = agent.get('https://smartlog.jp/')
+    page = agent.get('https://www.apple.com/jp/?afid=p238%7CsKSMckWTz-dc_mtid_18707vxu38484_pcrid_479606761351_pgrid_13140806301_&cid=aos-jp-kwgo-brand--slid---product--')
     # @elements = page
-    @elements = page.search()
+    @elements = page.search('.ac-gf-directory-column-section')
     # # @elements.get_attribute(:href)
     # # # @elements = page.search('')
 
-    @elements.each do |ele|
-      @hoge = ele[:href]
-      
-    end
+    # @elements.each do |ele|
+    #   @hoge = ele[:href]
+     
 
   end
 end
