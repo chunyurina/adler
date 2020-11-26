92eecc4bd4ebbf0a85c55a03f6aad1f48f1d3575
chunyurina <yurina8ru@gmail.com>
2020-11-26 20:50:33 +0900

    とりあえずのjs

--- app/javascript/yesno.js
+++ app/javascript/yesno.js
@@ -1,12 +1,58 @@
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'mike'
+// });
+// connection.connect();
+
+// console.log('Connected!');
+
+// connection.end();
+
+
+// ------------------------------------------------------------
+//      my sql connection
+// ------------------------------------------------------------
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'mike'
-});
-connection.connect();
 
-console.log('Connected!');
+// //DBの定義
+var dbConfig = {
+  host     : 'localhost', //接続先ホスト
+  user     : 'root',      //ユーザー名
+  password : '',    //パスワード
+  database : 'mike'     //DB名
+};
 
-connection.end();
+
+connection = mysql.createConnection(dbConfig);
+console.log('create mysql connection');
+console.log(connection);
+
+// function handleDisconnect() {
+//     console.log('create mysql connection');
+//     connection = mysql.createConnection(dbConfig); //接続する準備
+
+//     //接続
+//     connection.connect(function(err) {
+//         if(err) {
+//             console.log('error when connecting to db:', err);
+//             setTimeout(handleDisconnect, 2000); //2秒待ってから処理
+//         }
+//     });
+
+//     //error時の処理
+//     connection.on('error', function(err) {
+//         console.log('db error', err);
+//         if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//             handleDisconnect();
+//         } else {
+//             throw err;
+//         }
+//     });
+
+//     module.exports = connection; //connectionを(他のファイルから)requireで呼び出せるようにする
+// }
+
+// handleDisconnect();
