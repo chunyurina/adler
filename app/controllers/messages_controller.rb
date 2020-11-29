cf1bb6a929c61e117370f5b7ebbfcd9d5f8c4e48
Daisuke073 <cpxyh073@gmail.com>
2020-11-29 21:42:16 +0900

    user登録できました。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.new
-  @message = Message.find(1)
-  @first_answer = @message.first_answer
-  @second_answer = @message.second_answer
-  @third_answer = @message.third_answer
-  @article = Article.find(1)
+  @messages = Message.new
+  # @message = Message.find(1)
+  # @first_answer = @message.first_answer
+  # @second_answer = @message.second_answer
+  # @third_answer = @message.third_answer
+  # @article = Article.find(1)
   end
 
   def create
