0d0631bee948a8bbbc3c95e6e9074050c11f5584
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-12-05 11:58:40 +0900

    データの更新ができるようになりました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :messages, only: [:index, :create, :new] do
+  resources :messages, only: [:index, :create, :new, :edit, :update] do
     collection do
       get 'article', to: 'messages#article'
+      get 'edit', to: 'messages#edit'
     end
   end
   
