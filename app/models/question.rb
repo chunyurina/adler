ef98bb7dbab1429445909f1358ac6e53ff191151
chunyurina <yurina8ru@gmail.com>
2020-12-06 20:01:35 +0900

    フロントエンド（詳細）

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,3 @@
 class Question < ActiveHash::Base
-  self.data = [{id: 1, name: 'あなたは今落ち込んでいますか？'}, {id: 2, name: 'あなたは今やる気がありますか？'},{id:3, name: 'あなたは今仕事に行きたですか？'}]
+  self.data = [{id: 1, name: 'あなたは今落ち込んでいますか？'}, {id: 2, name: 'あなたは今やる気がありますか？'},{id:3, name: 'あなたは今仕事に行きたいですか？'}]
 end
